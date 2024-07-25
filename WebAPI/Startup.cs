40a23a02b34c7acf5dcae696a38db3e74447d3fa
AlpayKoyuncuoglu <41324656+AlpayKoyuncuoglu@users.noreply.github.com>
2024-07-26 00:12:43 +0300

    Product entitiy created and database connection occured

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,7 +11,9 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using WebAPI.Data;
 
 namespace WebAPI
 {
@@ -26,7 +29,17 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ProductContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("Local"));
+            });
+            //services.AddDbContext<ProductContext>(x =>
+            //{
+            //    x.UseSqlServer(services.Configuration.GetConnectionString("Local"), option =>
+            //    {
+            //        option.MigrationsAssembly(Assembly.GetAssembly(typeof(ProductContext)).GetName().Name);
+            //    });
+            //});
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
