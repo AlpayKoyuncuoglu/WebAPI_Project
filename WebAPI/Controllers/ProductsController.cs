d0a661221f9020f7436f305782ee62e2dace9753
AlpayKoyuncuoglu <41324656+AlpayKoyuncuoglu@users.noreply.github.com>
2024-07-25 22:27:54 +0300

    First Controller created

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[]
+            {
+                new { Name="Bilgisayar",Price=15000 },
+                new { Name="Telefon",Price=10000}
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok(new { Name = "Bilgisayar", Price = 15000 });
+        }
+    }
+}
