cb2b15efd917cc8c3c900e3f198c68ccfd1bda8a
piton_alpay <alpaykoyuncuoglu@piton.com.tr>
2024-07-26 10:02:11 +0300

    Data seeding

--- WebAPI/Data/ProductContext.cs
+++ WebAPI/Data/ProductContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAPI.Data
 {
@@ -7,6 +8,17 @@ namespace WebAPI.Data
         public ProductContext(DbContextOptions<ProductContext> options) : base(options)
         {
 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+            modelBuilder.Entity<Product>().HasData(new Product[]
+            {
+                   new(){Id=1,Name="Bilgisayar",CreatedDate=DateTime.Now.AddDays(-3),Price=15000,Stock=300},
+                   new(){Id=2,Name="Telefon",CreatedDate=DateTime.Now.AddDays(-30),Price=10000,Stock=500},
+                   new(){Id=3,Name="Klavye",CreatedDate=DateTime.Now.AddDays(-60),Price=1000,Stock=1000},
+            });
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<Product> Products4 { get; set; }
     }
---
+++ WebAPI/Migrations/20240726070030_SeedProductData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class SeedProductData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products4",
+                columns: new[] { "Id", "CreatedDate", "ImagePath", "Name", "Price", "Stock" },
+                values: new object[] { 1, new DateTime(2024, 7, 23, 10, 0, 29, 697, DateTimeKind.Local).AddTicks(4902), null, "Bilgisayar", 15000m, 300 });
+
+            migrationBuilder.InsertData(
+                table: "Products4",
+                columns: new[] { "Id", "CreatedDate", "ImagePath", "Name", "Price", "Stock" },
+                values: new object[] { 2, new DateTime(2024, 6, 26, 10, 0, 29, 701, DateTimeKind.Local).AddTicks(752), null, "Telefon", 10000m, 500 });
+
+            migrationBuilder.InsertData(
+                table: "Products4",
+                columns: new[] { "Id", "CreatedDate", "ImagePath", "Name", "Price", "Stock" },
+                values: new object[] { 3, new DateTime(2024, 5, 27, 10, 0, 29, 701, DateTimeKind.Local).AddTicks(785), null, "Klavye", 1000m, 1000 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products4",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products4",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products4",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
